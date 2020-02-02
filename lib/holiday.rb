e26e6ac953f9ebd7b953859f93f3b289ff6f1b0a
Kwane Smith <smithkwane6@gmail.com>
2020-02-02 23:16:10 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,6 +27,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   holiday_hash[:winter].each do |holiday, original_supply|
     original_supply << supply
+  end
 end
 
 
